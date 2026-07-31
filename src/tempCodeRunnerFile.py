92c4881db3976a50f7df9b97cc94bdbb964e25f7
Gustavo Franz <196722369+GustaFranz@users.noreply.github.com>
2026-07-31 19:24:56 -0300

    renoção de documentos temporarios 2

--- src/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-from src.leitor_projetos import ler_projetos, transformar_projetos_para_longo
-from src.leitor_provas import ler_provas, transformar_provas_para_longo
-from src.leitor_simulados import ler_simulados, transformar_simulados_para_longo
--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-# Aba 6º ano: Point(x=351, y=459)
-# Baixar CSV 6º ano: Point(x=349, y=407)
-
-# Aba 7º ano: Point(x=808, y=462)
-# Baixar CSV 7º ano: Point(x=349, y=407)
-
-# Aba 8º ano: Point(x=1268, y=459)
-# Baixar CSV 8º ano: Point(x=349, y=407)
