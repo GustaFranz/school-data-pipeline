178bdc4a7dca1d2fc0cd63f6b81571a7f96ade93
Gustavo Franz <196722369+GustaFranz@users.noreply.github.com>
2026-08-27 16:27:50 -0300

    criei arquivos para testar/estudar/relembrar a função melt do pandas que transforma de largo para longo

--- notebooks/df_arquivos_brutos.py
+++ notebooks/df_arquivos_brutos.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 ##OBSERVAÇÃO: O código abaixo é apenas um teste para ler o arquivo simulado_6ano.csv, que está na pasta dados/simulados. Ele não faz parte do código final do projeto.
 ##Para ler o data frame de outras séries como 7º e 8º anos, basta alterar o nome do arquivo no caminho da variável pasta_simulados_csv.
-pasta_notas_csv = Path(__file__).resolve().parent.parent.parent
+pasta_notas_csv = Path(__file__).resolve().parent.parent
 pasta_simulados_csv = pasta_notas_csv / "dados" / "simulados" / "simulado_6ano.csv"
 
 df_arquivos = pd.read_csv(pasta_simulados_csv)
--- notebooks/transformar_largo.py
+++ notebooks/transformar_largo.py
@@ -1,8 +1,12 @@
 import pandas as pd
 from pathlib import Path
 
+pd.set_option("display.max_rows", None)
+'''
+Possibilita verificar quantidade linhas quase ilimitadas no hear()
+'''
 
-pasta_notas_csv = Path(__file__).resolve().parent.parent.parent
+pasta_notas_csv = Path(__file__).resolve().parent.parent
 pasta_simulados_csv = pasta_notas_csv / "dados" / "simulados"
 
 def unir_simulados():
@@ -17,5 +21,36 @@ def unir_simulados():
     return pd.concat(lista_simulados, ignore_index=True)
 
 
-df_simulados = unir_simulados()
-print(df_simulados)
+def transformar_simulado_para_longo(simulados):
+    """
+Transforma a tabela do formato largo para o formato longo usando melt.
+As colunas "turma" e "aluno" são mantidas fixas por meio de id_vars.
+Todas as demais colunas são convertidas em linhas:
+os nomes dessas colunas passam a ser armazenados em "disciplina", definido por var_name;
+os valores contidos nelas passam a ser armazenados em "nota_simulado", definido por value_name.
+Exemplo conceitual:
+Antes:
+    turma | aluno | matematica | portugues
+    
+Depois:
+    turma | aluno | disciplina | nota_simulado
+"""
+    return simulados.melt(
+        # var_name: nomeia a nova coluna que identificará o que eram as colunas originais, 
+        # já que elas serão transformadas em linhas.
+        id_vars=["turma", "aluno"],
+        # value_name: nomeia a nova coluna que armazenará os valores que antes estavam 
+        # distribuídos nessas colunas originais.
+        var_name="disciplina",
+        value_name="nota_simulado"
+    )
+
+
+simulados = unir_simulados()
+simulados_longos = transformar_simulado_para_longo(simulados)
+print(simulados_longos.head(75))
+#Coloquei head(145) para verificar que:
+#os primeiros alunos: Ana e Bruno aparecem no começo da lista do 6º ano para a disciplina  Lingua Portuguesa
+#os mesmos alunos aparecem no começo da lisra para a disciplina Matemática (nos índice 71 e 72)
+#lembrar que normalmente o .head() não suporta numero alto como 70, então usei:
+#pd.set_option("display.max_rows", None) que permite isso -> no topo do aqruivo
